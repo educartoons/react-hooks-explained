08fa2639caffcabcdb53d355dc79a0d01f3e4880
Eduar <educartoons@gmail.com>
2019-09-19 00:17:59 -0500

    useState - params like an object

--- src/components/state/State.js
+++ src/components/state/State.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 
 export const StateComponent = () => {
 
-  const [count, setCount] = useState(10);
+  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
 
   return (
     <div>
       <h2>useState</h2>
-      <p>Value: {count}</p>
-      <button onClick={e => setCount((currentValue) => currentValue + 1)}>Count !</button>
+      <p>Count 1: {count}</p>
+      <p>Count 2: {count2}</p>
+      <button onClick={e => setCount((currentState) => Object.assign({}, currentState, { count: count + 1 }))}>Count !</button>
     </div>
   )
 }
